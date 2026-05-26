0c6126ddd7953b760e00764829b3e9bdf8db2753
Your Name <your.email@example.com>
2026-05-26 15:21:27 +0300

    Added a json file for persistence

--- store.py
+++ store.py
@@ -1,11 +1,25 @@
 # Brain of the mini database
+import json
+from pathlib import Path
 
 class MiniRedisStore:
-    def __init__(self):
-        self.store={}
+    def __init__(self, filename="data.json"):
+        self.filename= Path(filename)
+        self.store=self.load()
+  
+    def load(self):
+        if self.filename.exists():
+            with open(self.filename, "r") as file:
+                return json.load(file)
+        return  {} 
+
+    def save(self):
+        with open(self.filename, "w")as file:
+            json.dump(self.store,file)
 
     def set(self, key, value):
         self.store[key]=value
+        self.save()
         return "OK"
     
     def get(self, key):
@@ -14,6 +28,7 @@ class MiniRedisStore:
     def delete(self,key):
         if key in self.store:
             del self.store[key]
+            self.save()
             return "OK"
         return "(nil)"
     
